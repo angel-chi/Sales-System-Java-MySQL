6633910fcbb06d53ffad81735d90e171ade9a634
Jose Rodolfo Hernandez Escamilla <a24216390@alumnos.uady.mx>
2025-12-05 17:01:05 -0600

    pruebaINTELIJ, no cambia nada

--- src/main/java/org/borghisales/salessysten/controllers/MenuController.java
+++ src/main/java/org/borghisales/salessysten/controllers/MenuController.java
@@ -47,7 +47,6 @@ public class MenuController {
 
 
             stage.setResizable(false); // Evite maximizar
-            stage.setMaximized(true);
 
 
             configureStageCloseEvent(stage, fxmlFileName, title);
