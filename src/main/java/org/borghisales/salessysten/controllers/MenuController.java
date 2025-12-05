f8ab2e51d91bccc55dddc274216eda5ae0374687
Jose Rodolfo Hernandez Escamilla <a24216390@alumnos.uady.mx>
2025-12-04 21:11:26 -0600

    Actualización MenuController, retirando el poder maximizar, eliminando el método openNewStage
    Se implementa una modificación en MenuController, un listener de stage para evitar que el usuario pueda maximizar las ventanas en la interfaz, proporcionando un ámbito estético. A su vez se implementa el openAutoSizeStage al cerrar una ventana hija y abriendo de una manera mejor su ventana padre.

--- src/main/java/org/borghisales/salessysten/controllers/MenuController.java
+++ src/main/java/org/borghisales/salessysten/controllers/MenuController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -22,81 +23,73 @@ public class MenuController {
     public static final String REPORT_VIEW_FXML = VIEWS_DIRECTORY + "ReportsView.fxml";
     public static final String SALE_DETAIL_VIEW_FXML = VIEWS_DIRECTORY + "SaleDetailView.fxml";
 
-    static Alert defaultAlert;
-    static ButtonType acceptButton = new ButtonType("Aceptar");
 
-    public static HashMap<String, String> filePaths = new HashMap<>();
+    static Alert defaultAlert;
+    static ButtonType acceptButton = new ButtonType("Accept");
+    
+    public static HashMap<String, String > filePaths = new HashMap<>();
 
     void closeCurrentStage(Node node) {
+
         Stage stage = (Stage) node.getScene().getWindow();
         stage.close();
     }
 
-    public void openNewStage(String fxmlFileName, String title) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(MenuController.class.getResource(fxmlFileName));
-            Scene scene = new Scene(fxmlLoader.load());
-            Stage stage = new Stage();
-            stage.setTitle(title);
-            stage.setScene(scene);
-
-            stage.setMinWidth(305);
-            stage.setMinHeight(415);
-
-            stage.setWidth(305);
-            stage.setHeight(415);
-
-            configureStageCloseEvent(stage, fxmlFileName, title);
-            stage.show();
-            stage.centerOnScreen();
-
-        } catch (IOException | NullPointerException e){
-            setAlert(Alert.AlertType.WARNING, "Error para cargar la ventana: " + e.getMessage());
-        }
-    }
 
     public void openAutoSizeStage(String fxmlFileName, String title) {
         try {
+            // Abrir una nueva ventana (core del sistema)
             FXMLLoader fxmlLoader = new FXMLLoader(MenuController.class.getResource(fxmlFileName));
             Scene scene = new Scene(fxmlLoader.load());
             Stage stage = new Stage();
             stage.setTitle(title);
             stage.setScene(scene);
 
+
+            stage.setResizable(false); // Evite maximizar
             stage.setMaximized(true);
+
 
             configureStageCloseEvent(stage, fxmlFileName, title);
             stage.show();
             stage.centerOnScreen();
 
-        } catch (IOException | NullPointerException e){
-            setAlert(Alert.AlertType.WARNING, "Error para cargar la ventana: " + e.getMessage());
+        } catch (IOException | NullPointerException e) {
+            setAlert(Alert.AlertType.WARNING, "Error loading the view: "+ e.getMessage());
         }
     }
 
     private void configureStageCloseEvent(Stage stage, String fxmlFileName, String title) {
+        /*
+        Acción al cerrar una ventana.
+        Si NO estás en la ventana principal
+        Cuando el usuario cierra la ventana
+        Automáticamente se abre su ventana padre
+         */
         if (!fxmlFileName.equals(MAIN_VIEW_FXML)) {
             stage.setOnCloseRequest(e -> {
-                openNewStage(getFxmlFather(fxmlFileName), title);
+                openAutoSizeStage(getFxmlFather(fxmlFileName),title);
             });
         }
     }
-
-    String getFxmlFather(String fxml) {
+    // Obtener la vista padre
+    String getFxmlFather(String fxml){
         return filePaths.get(fxml);
     }
-
-    public static void setAlert(Alert.AlertType alertType, String argument) {
+    // Alertas globales
+    static public void setAlert(Alert.AlertType alertType,String argument){
         defaultAlert = new Alert(alertType);
-        defaultAlert.setTitle("Información");
+        defaultAlert.setTitle("Information");
         defaultAlert.setHeaderText(null);
         defaultAlert.getButtonTypes().setAll(acceptButton);
         defaultAlert.setContentText(argument);
         defaultAlert.showAndWait();
     }
-
-    public static void cleanCells(TextField...cells) {
-        for (TextField e:cells)
+    // Limpia múltiples campos en una sola línea
+    static public void cleanCells(TextField...cells){
+        for (TextField e : cells)
             e.clear();
     }
+
+
 }
