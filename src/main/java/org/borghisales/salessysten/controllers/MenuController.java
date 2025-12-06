5b8e4454c4ee0eb638f34e8809e4d1412f533a69
Victor-Gonzalez-Nahuat <a24216377@alumnos.uady.mx>
2025-12-06 12:39:20 -0600

    Mejora UI / Fixed: Mostrar alerts siempre encima

--- src/main/java/org/borghisales/salessysten/controllers/MenuController.java
+++ src/main/java/org/borghisales/salessysten/controllers/MenuController.java
@@ -1,15 +1,19 @@
 package org.borghisales.salessysten.controllers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Optional;
 
 public class MenuController {
 
@@ -46,11 +50,11 @@ public class MenuController {
             stage.setWidth(700);
             stage.setHeight(650);
             stage.setAlwaysOnTop(true);
-
         } catch (IOException | NullPointerException e) {
             setAlert(Alert.AlertType.WARNING, "Error loading the view: "+ e.getMessage());
         }
     }
+
 
     private void configureStageCloseEvent(Stage stage, String fxmlFileName, String title) {
         if (!fxmlFileName.equals(MAIN_VIEW_FXML)) {
@@ -64,14 +68,46 @@ public class MenuController {
         return filePaths.get(fxml);
     }
 
-    static public void setAlert(Alert.AlertType alertType,String argument){
-        defaultAlert = new Alert(alertType);
-        defaultAlert.setTitle("Information");
-        defaultAlert.setHeaderText(null);
-        defaultAlert.getButtonTypes().setAll(acceptButton);
-        defaultAlert.setContentText(argument);
-        defaultAlert.showAndWait();
+    static public void setAlert(Alert.AlertType alertType, String argument) {
+        // SOLUCION -> Alertas siempre encima
+        //Se espera a que el hilo este disponible para evitar problemas de interfaz
+        Platform.runLater(() -> {
+            Alert alert = new Alert(alertType);
+            alert.setTitle("Información");
+            alert.setHeaderText(null);
+            alert.getButtonTypes().setAll(acceptButton);
+            alert.setContentText(argument);
+
+            //Trata de buscar una ventana padre (que este activa)
+            Optional<Window> owner = Window.getWindows().stream()
+                    .filter(Window::isFocused)
+                    .findFirst();
+
+            //Si no hay ventana activa busca la primera que se este mostrando
+            if (!owner.isPresent()) {
+                owner = Window.getWindows().stream()
+                        .filter(Window::isShowing)
+                        .findFirst();
+            }
+            //Asignar ventana padre a la alerta
+            owner.ifPresent(alert::initOwner);
+            //Activa el modal para bloquear
+            alert.initModality(Modality.APPLICATION_MODAL);
+
+            //Cast con stage para aplicar alwaysOnTop
+            Stage alertStage = (Stage) alert.getDialogPane().getScene().getWindow();
+            alertStage.setAlwaysOnTop(true);
+
+            // Mostrar y esperar al cierre, al cierre el stage es eliminado
+            alert.showAndWait();
+
+            // Cualquier cosa tratar de quitar el alwaysOntTop
+            try {
+                alertStage.setAlwaysOnTop(false);
+            } catch (Exception ignored) {}
+        });
     }
+
 
     static public void cleanCells(TextField...cells){
         for (TextField e:cells)
