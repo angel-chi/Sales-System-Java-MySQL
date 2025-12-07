9bbdf7ac0ffde08a44ab1973f0d61f4264761aa7
DiegoZapata619 <diegoarminzapata@gmail.com>
2025-12-07 12:32:03 -0600

    Correción de nombres.

--- src/main/java/org/borghisales/salessysten/controllers/MainController.java
+++ src/main/java/org/borghisales/salessysten/controllers/MainController.java
@@ -31,7 +31,7 @@ public class MainController extends MenuController implements Initializable {
     @FXML
     private void signIn(){
         if (SellerDAO.login(user.getText(),password.getText())) {
-            openNewStage(MANAGEMENT_VIEW_FXML, "Administración");
+            openNewStage(MANAGEMENT_VIEW_FXML, "Gestión");
             closeCurrentStage(user);
         }
     }
